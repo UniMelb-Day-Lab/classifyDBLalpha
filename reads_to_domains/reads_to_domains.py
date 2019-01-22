18d581a71f855c4f78987944f7c5625f38a1cfff
Gerry Tonkin-Hill <gqt20@cam.ac.uk>
2019-01-22 06:54:08 +0000

    add command line arguments for reads_to_domains script

--- reads_to_domains/reads_to_domains.py
+++ reads_to_domains/reads_to_domains.py
@@ -1,18 +1,38 @@
 from mungo.fasta import FastaReader
 from collections import defaultdict
+from optparse import OptionParser
 
-top_hits = defaultdict(lambda: ("", 100.0))
 
-#Get top hits for each sequence
-for line in open("InsertFilename_nhmmOut.txt", 'rU'):
- if line[0]=="#": continue
- line=line.strip().split()
- read = line[0]
- E = float(line[4])
- if  E < top_hits[read][1]:
-   top_hits[read] = (line[2], E)
 
-with open("reads_to_domains.csv", 'w') as outfile:
- outfile.write("read,domain,e-value\n")
- for read in top_hits:
-   outfile.write(read+","+top_hits[read][0]+","+str(top_hits[read][1])+"\n")
+def main():
+    parser = OptionParser()
+
+    parser.add_option("", "--hmm", dest="hmmout",
+        help="the nhmmOut file from running allocate_reads.py")
+
+    parser.add_option("-o", "--out", dest="outfile",
+        help="the location of an output file")
+
+    (options, args) = parser.parse_args()
+
+    top_hits = defaultdict(lambda: ("", 100.0))
+
+    #Get top hits for each sequence
+    for line in open(options.hmmout, 'rU'):
+        if line[0]=="#": continue
+        line=line.strip().split()
+        read = line[0]
+        E = float(line[4])
+        if  E < top_hits[read][1]:
+            top_hits[read] = (line[2], E)
+
+    with open(options.outfile, 'w') as outfile:
+     outfile.write("read,domain,e-value\n")
+     for read in top_hits:
+       outfile.write(read+","+top_hits[read][0]+","+str(top_hits[read][1])+"\n")
+
+    return
+
+
+if __name__ == '__main__':
+    main()
